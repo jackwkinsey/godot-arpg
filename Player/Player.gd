6513c307fc8255884883e3f4f924bd81ef8da199
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-04-26 22:03:16 -0400

    feat: add simple animation logic to player script

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,6 +6,8 @@ const FRICTION = 500;
 
 var velocity = Vector2.ZERO;
 
+onready var animation_player = $AnimationPlayer;
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
@@ -13,8 +15,14 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized();
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animation_player.play("RunRight");
+		else:
+			animation_player.play("RunLeft");
+		
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
+		animation_player.play("IdleRight");
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 		
 	velocity = move_and_slide(velocity);
