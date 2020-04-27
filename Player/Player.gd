40076d87d0bb7457a3df964122eb6b83d8549457
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-04-26 22:47:03 -0400

    feat: add animation transitions for idle and run

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,13 @@
 extends KinematicBody2D
 
-const ACCELERATION = 500;
+const ACCELERATION = 600;
 const MAX_SPEED = 80;
-const FRICTION = 500;
+const FRICTION = 600;
 
 var velocity = Vector2.ZERO;
 
-onready var animation_player = $AnimationPlayer;
+onready var animation_tree = $AnimationTree;
+onready var animation_state = animation_tree.get("parameters/playback");
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
@@ -15,14 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized();
 	
 	if input_vector != Vector2.ZERO:
-		if input_vector.x > 0:
-			animation_player.play("RunRight");
-		else:
-			animation_player.play("RunLeft");
-		
+		animation_tree.set("parameters/Idle/blend_position", input_vector);
+		animation_tree.set("parameters/Run/blend_position", input_vector);
+		animation_state.travel("Run");
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
-		animation_player.play("IdleRight");
+		animation_state.travel("Idle");
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 		
 	velocity = move_and_slide(velocity);
