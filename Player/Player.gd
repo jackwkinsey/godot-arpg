7f817ff61cdc2078ffdc1f28f61539b9541c73d1
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-04-26 20:20:34 -0400

    feat: improve movement code

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,9 @@
 extends KinematicBody2D
 
+const ACCELERATION = 20;
+const MAX_SPEED = 100;
+const FRICTION = 20;
+
 var velocity = Vector2.ZERO;
 
 func _physics_process(delta):
@@ -7,6 +11,10 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
 	
-	velocity = input_vector;
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector.normalized() * ACCELERATION * delta;
+		velocity = velocity.clamped(MAX_SPEED * delta);
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 		
 	move_and_collide(velocity);
