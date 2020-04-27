35da1fb8699626654f7e56eeca52998c48cf4a9a
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-04-26 21:05:19 -0400

    feat: add basic collisions

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 20;
-const MAX_SPEED = 100;
-const FRICTION = 20;
+const ACCELERATION = 500;
+const MAX_SPEED = 80;
+const FRICTION = 500;
 
 var velocity = Vector2.ZERO;
 
@@ -10,11 +10,11 @@ func _physics_process(delta):
 	var input_vector = Vector2.ZERO;
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+	input_vector = input_vector.normalized();
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector.normalized() * ACCELERATION * delta;
-		velocity = velocity.clamped(MAX_SPEED * delta);
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta);
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
 		
-	move_and_collide(velocity);
+	velocity = move_and_slide(velocity);
