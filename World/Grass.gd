e58ca87ab624f19b70b47bfcd83de2e6d4d178a3
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-04-28 22:38:38 -0400

    feat: add hurtboxes to grass and hurt logic
    Grass objects now have a hurtbox that will trigger them being destroyed
    when they come in contact with a hitbox. I think it actually works when
    contacting *ANY* Area2D node but for now that is just another hitbox.

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
-		var grass_effect = load("res://Effects/GrassEffect.tscn").instance();
-		var world = get_tree().current_scene;
-		world.add_child(grass_effect);
-		grass_effect.global_position = global_position;
-		queue_free();
+func create_grass_effect():
+	var grass_effect = load("res://Effects/GrassEffect.tscn").instance();
+	var world = get_tree().current_scene;
+	world.add_child(grass_effect);
+	grass_effect.global_position = global_position;
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect();
+	queue_free();
