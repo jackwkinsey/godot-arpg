f9b130349ec540c8acac36aca8154973d3823ca3
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-04-28 21:58:42 -0400

    feat: add grass fall apart effect
    This effect is added at the grass location when it is destroyed. It then
    plays its animation and then destroys itself after a decay time.

---
+++ Effects/GrassEffect.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var life_time = 1;
+var decay = false;
+
+onready var animated_sprite = $AnimatedSprite;
+
+func _ready():
+	animated_sprite.frame = 0;
+	animated_sprite.play("FallApart");
+	
+func _process(delta):
+	life_time -= delta;
+	
+	if (decay && life_time <= 0):
+		queue_free();
+
+func _on_AnimatedSprite_animation_finished():
+	decay = true;
